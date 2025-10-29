222095821b7a41e4b561f9a9af836cec7b45acd4
AlokGDAAS <ankit25792kumar@gmail.com>
2025-10-29 09:22:33 +0530

    push code first time

---
+++ my-app/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="bg-[var(--primary)] p-1">
+        <div className="w-9/12 mx-auto flex justify-between ">
+          <div className="">
+            <img src="./ganesh.png" alt="ganesh ji" className="w-30" />
+          </div>
+
+          <div></div>
+          <div>
+            <img src="./lakshmi.png" alt="laxmi ji" className="w-30 aspect-1/1" />
+          </div>
+        </div>
+      </div>
+
+      <div className="w-9/12 mx-auto ">
+        <ul className="flex px-4 py-1 gap-4 justify-center font-semibold text-lg ">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
